0a9a025e744da09393fb19911590319ac31e2d4e
NOKUBI Takatsugu <knok@daionet.gr.jp>
2018-02-26 15:55:30 +0900

    download script

---
+++ ansible/download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -c https://pdollar.github.io/toolbox/archive/piotr_toolbox.zip
+wget -c https://ftp.gnu.org/gnu/octave/octave-4.2.1.tar.xz
+wget -c https://raw.githubusercontent.com/pdollar/edges/master/private/edgesNmsMex.cpp
+
